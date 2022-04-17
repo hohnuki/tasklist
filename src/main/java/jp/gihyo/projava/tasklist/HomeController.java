a773aa6075336451193ec5fb8a9702f6dbf7d7da
大貫　弘貴 <ohnukihiroki8585@yahoo.co.jp>
2022-04-17 21:12:06 +0900

    datebaseと繋がった

--- src/main/java/jp/gihyo/projava/tasklist/HomeController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeController.java
@@ -1,5 +1,6 @@
 package jp.gihyo.projava.tasklist;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,12 @@ import java.util.UUID;
 
 @Controller
 public class HomeController {
+    private final TaskListDao dao;
+
+    @Autowired
+    HomeController(TaskListDao dao) {
+        this.dao = dao;
+    }
     record TaskItem(String id, String task, String deadline, boolean done) {
     }
 
@@ -26,6 +33,7 @@ public class HomeController {
 
     @GetMapping(value = "/list")
     String listItems(Model model) {
+        List<TaskItem> taskItems = dao.findAll();
         model.addAttribute("taskList", taskItems);
         return "home";
     }
@@ -36,6 +44,7 @@ public class HomeController {
         String id = UUID.randomUUID().toString().substring(0, 8);
         TaskItem item = new TaskItem(id, task, deadline, false);
         taskItems.add(item);
+        dao.add(item);
         return "redirect:/list";
     }
 }
